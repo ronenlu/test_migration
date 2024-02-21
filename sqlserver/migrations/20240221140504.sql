25847b9effdf10cf1a54d4d5cb41b29bd1f3dfd0
Ronen Lubin <ronen7822@gmail.com>
2024-02-21 16:08:33 +0200

    test atlas action in with sqlserver

---
+++ sqlserver/migrations/20240221140504.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Customers (
+    First_Name varchar(50) NOT NULL,
+    Last_Name varchar(50) NOT NULL,
+    City varchar(50) NOT NULL,
+    Email varchar(100) NOT NULL,
+    Registration_Date date NOT NULL
+);
