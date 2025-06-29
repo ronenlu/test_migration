ed0699a717288f1a3396c50781aa1c50f4e9f03a
Ronen Lubin <ronen7822@gmail.com>
2025-06-29 16:03:27 +0300

    -add nolint directive

--- migrations/20250629115627_v8_drop_column.sql
+++ migrations/20250629115627_v8_drop_column.sql
@@ -1,8 +1,2 @@
--- atlas:txtar
--- checks/destructive.sql --
--- atlas:assert DS103
-SELECT NOT EXISTS (SELECT 1 FROM `Reviews` WHERE `Rating` IS NOT NULL) AS `is_empty`;
--- migration.sql --
--- Drop column "Rating" from table "Reviews"
--- Drop column "Rating" from table "Reviews"
+-- atlas:nolint DS103
 ALTER TABLE `Reviews` DROP COLUMN `Rating`;
