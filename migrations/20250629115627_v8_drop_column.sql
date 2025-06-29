e124ecf6d9b419b9f5e25f0e623360ed9de100de
Ronen Lubin <ronen7822@gmail.com>
2025-06-29 15:41:08 +0300

    add pre migration check for empty column

--- migrations/20250629115627_v8_drop_column.sql
+++ migrations/20250629115627_v8_drop_column.sql
@@ -1,2 +1,8 @@
+-- atlas:txtar
+-- checks/destructive.sql --
+-- atlas:assert DS103
+SELECT NOT EXISTS (SELECT 1 FROM `Reviews` WHERE `Rating` IS NOT NULL) AS `is_empty`;
+-- migration.sql --
+-- Drop column "Rating" from table "Reviews"
 -- Drop column "Rating" from table "Reviews"
 ALTER TABLE `Reviews` DROP COLUMN `Rating`;
