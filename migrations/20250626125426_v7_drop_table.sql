0acc810c8f5962541ea17ae985ef132ca0b6d848
Ronen Lubin <ronen7822@gmail.com>
2025-10-19 11:18:03 +0300

    m

--- migrations/20250626125426_v7_drop_table.sql
+++
@@ -1,4 +0,0 @@
--- Drop index named "IDX_OrdersByDate_TotalAmount"
-DROP INDEX `IDX_OrdersByDate_TotalAmount`;
--- Drop table named "OrdersByDate"
-DROP TABLE `OrdersByDate`;
--- migrations/20250630075631_v9_drop_more_column.sql
+++
@@ -1,9 +0,0 @@
--- atlas:txtar
-
--- checks/destructive.sql --
--- atlas:assert DS103
-SELECT IF (EXISTS (SELECT 1 FROM `Reviews` WHERE `Comment` IS NOT NULL), 0, 1) AS `is_empty`;
-
--- migration.sql --
--- Drop column "Comment" from table "Reviews"
-ALTER TABLE `Reviews` DROP COLUMN `Comment`;
